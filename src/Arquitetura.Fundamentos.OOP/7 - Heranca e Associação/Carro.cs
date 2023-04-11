afec55cf1f38a5a73db4a3716491e9c47d484114
Icaro Milet <milet.icaro@gmail.com>
2023-04-11 10:41:45 -0300

    feat: oop associação e herança

---
+++ src/Arquitetura.Fundamentos.OOP/7 - Heranca e Associação/Carro.cs
@@ -0,0 +1,15 @@
+﻿namespace Arquitetura.Fundamentos.OOP._7___Heranca_e_Associação
+{
+    public class Carro
+    {
+        private int ano;
+        private string marca;
+        private Motor motor;
+
+        public void Acelerar() 
+        {
+            motor.Ligar();
+            motor.Combustao();
+        }
+    }
+}
---
+++ src/Arquitetura.Fundamentos.OOP/7 - Heranca e Associação/Motor.cs
@@ -0,0 +1,12 @@
+﻿namespace Arquitetura.Fundamentos.OOP._7___Heranca_e_Associação
+{
+    public class Motor
+    {
+        private int cilindrada;
+        private bool motorFlex;
+
+        public void Ligar() { }
+
+        public void Combustao() { }
+    }
+}
