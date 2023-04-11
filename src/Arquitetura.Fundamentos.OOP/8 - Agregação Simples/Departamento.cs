8407509f1d0de11495fff39c8b8918ea5e14100d
Icaro Milet <milet.icaro@gmail.com>
2023-04-11 13:39:32 -0300

    feat: oop agregação simples

---
+++ src/Arquitetura.Fundamentos.OOP/8 - Agregação Simples/Departamento.cs
@@ -0,0 +1,7 @@
+﻿namespace Arquitetura.Fundamentos.OOP._8___Agregação_Simples
+{
+    public class Departamento
+    {
+
+    }
+}
---
+++ src/Arquitetura.Fundamentos.OOP/8 - Agregação Simples/Universidade.cs
@@ -0,0 +1,8 @@
+﻿namespace Arquitetura.Fundamentos.OOP._8___Agregação_Simples
+{
+    public class Universidade
+    {
+        private string nome;
+        private Departamento[] departamentos;
+    }
+}
